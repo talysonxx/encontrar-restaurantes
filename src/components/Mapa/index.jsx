ed122ef7f8d044434cdd55b89f784fdfb8c8ccf3
talysonxx <talysonoficial12@gmail.com>
2021-08-16 11:35:48 -0300

    map

--- src/components/Mapa/index.jsx
+++ src/components/Mapa/index.jsx
@@ -4,10 +4,10 @@ import {GoogleApiWrapper, Map, Marker} from 'google-maps-react'
 export const MapContainer = (props) => {
     const {google} = props
 
-    return <Map google={google} centerAroundCurrentLocation/>
+    return <Map googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCwbj4fmqpd2E678L4HM6xD_pbT9qcp_hc&v=3.exp&libraries=geometry,drawing,places`} google={google} centerAroundCurrentLocation/>
 }
 
 export default GoogleApiWrapper({
     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-    language: 'pt-BR',
+    language: 'pt-BR'
 })(MapContainer)
